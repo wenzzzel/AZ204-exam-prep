3cb1946da8d5630293c4d724a7a883d0afb8fde7
wenzzzel <wenzzzel@live.se>
2022-09-05 15:21:10 +0200

    feat: Setup a basic chained function

--- Azure-durable-functions/Azure-durable-function-Chaining/ChainingFunction.cs
+++ Azure-durable-functions/Azure-durable-function-Chaining/ChainingFunction.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -11,36 +12,63 @@ namespace Azure_durable_function_Chaining
 {
     public static class ChainingFunction
     {
-        [FunctionName("Function1")]
-        public static async Task<List<string>> RunOrchestrator(
+
+        
+
+        [FunctionName("Chaining")]
+        public static async Task<object> RunOrchestrator(
             [OrchestrationTrigger] IDurableOrchestrationContext context)
         {
-            var outputs = new List<string>();
-
-            // Replace "hello" with the name of your Durable Activity Function.
-            outputs.Add(await context.CallActivityAsync<string>("Function1_Hello", "Tokyo"));
-            outputs.Add(await context.CallActivityAsync<string>("Function1_Hello", "Seattle"));
-            outputs.Add(await context.CallActivityAsync<string>("Function1_Hello", "London"));
-
-            // returns ["Hello Tokyo!", "Hello Seattle!", "Hello London!"]
-            return outputs;
+            //try
+            //{
+                var x = await context.CallActivityAsync<object>("Function1", 0);
+                var y = await context.CallActivityAsync<object>("Function2", x);
+                var z = await context.CallActivityAsync<object>("Function3", y);
+                return await context.CallActivityAsync<object>("Function4", z);
+            //}
+            //catch (Exception)
+            //{
+            //    return null;
+            //}
         }
 
-        [FunctionName("Function1_Hello")]
-        public static string SayHello([ActivityTrigger] string name, ILogger log)
+        [FunctionName("Function1")]
+        public static int Function1([ActivityTrigger] int baseValue)
         {
-            log.LogInformation($"Saying hello to {name}.");
-            return $"Hello {name}!";
+            return baseValue + 1;
         }
 
-        [FunctionName("Function1_HttpStart")]
-        public static async Task<HttpResponseMessage> HttpStart(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestMessage req,
-            [DurableClient] IDurableOrchestrationClient starter,
+        [FunctionName("Function2")]
+        public static int Function2([ActivityTrigger] int baseValue)
+        {
+            return baseValue + 2;
+        }
+
+        [FunctionName("Function3")]
+        public static int Function3([ActivityTrigger] int baseValue)
+        {
+            return baseValue + 3;
+        }
+
+        [FunctionName("Function4")]
+        public static int Function4([ActivityTrigger] int baseValue)
+        {
+            return baseValue + 4;
+        }
+    }
+
+    public static class HttpStart
+    {
+        [FunctionName("HttpStart")]
+        public static async Task<HttpResponseMessage> Run(
+            [HttpTrigger(AuthorizationLevel.Function, methods: "post", Route = "orchestrators/{functionName}")] HttpRequestMessage req,
+            [DurableClient] IDurableClient starter,
+            string functionName,
             ILogger log)
         {
             // Function input comes from the request content.
-            string instanceId = await starter.StartNewAsync("Function1", null);
+            object eventData = await req.Content.ReadAsAsync<object>();
+            string instanceId = await starter.StartNewAsync(functionName, eventData);
 
             log.LogInformation($"Started orchestration with ID = '{instanceId}'.");
 
