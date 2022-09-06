32f955e822784afb4e6e5c4471eb4fbb239176ef
Erik Wenzel <erik.wenzel@volvocars.com>
2022-09-06 15:54:12 +0200

    feat: Setup azure durable function according to fan-in/fan-out pattern

--- Azure-durable-functions/Azure-durable-function-Fan-out-fan-in/E2_BackupSiteContent.cs
+++ Azure-durable-functions/Azure-durable-function-Fan-out-fan-in/E2_BackupSiteContent.cs
@@ -1,4 +1,6 @@
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
@@ -6,41 +8,88 @@ using Microsoft.Azure.WebJobs.Extensions.DurableTask;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
+using Microsoft.WindowsAzure.Storage.Blob;
+
 
 namespace Azure_durable_function_Fan_out_fan_in
 {
     public static class E2_BackupSiteContent
     {
         [FunctionName("E2_BackupSiteContent")]
-        public static async Task<List<string>> RunOrchestrator(
-            [OrchestrationTrigger] IDurableOrchestrationContext context)
+        public static async Task<long> Run(
+            [OrchestrationTrigger] IDurableOrchestrationContext backupContext)
         {
-            var outputs = new List<string>();
+            string rootDirectory = backupContext.GetInput<string>()?.Trim();
+            if (string.IsNullOrEmpty(rootDirectory))
+            {
+                rootDirectory = Directory.GetParent(typeof(E2_BackupSiteContent).Assembly.Location).FullName;
+            }
 
-            // Replace "hello" with the name of your Durable Activity Function.
-            outputs.Add(await context.CallActivityAsync<string>("Function1_Hello", "Tokyo"));
-            outputs.Add(await context.CallActivityAsync<string>("Function1_Hello", "Seattle"));
-            outputs.Add(await context.CallActivityAsync<string>("Function1_Hello", "London"));
+            string[] files = await backupContext.CallActivityAsync<string[]>(
+                "E2_GetFileList",
+                rootDirectory);
 
-            // returns ["Hello Tokyo!", "Hello Seattle!", "Hello London!"]
-            return outputs;
+            var tasks = new Task<long>[files.Length];
+            for (int i = 0; i < files.Length; i++)
+            {
+                tasks[i] = backupContext.CallActivityAsync<long>(
+                    "E2_CopyFileToBlob",
+                    files[i]);
+            }
+
+            await Task.WhenAll(tasks);
+
+            long totalBytes = tasks.Sum(t => t.Result);
+            return totalBytes;
         }
 
-        [FunctionName("Function1_Hello")]
-        public static string SayHello([ActivityTrigger] string name, ILogger log)
+        [FunctionName("E2_GetFileList")]
+        public static string[] GetFileList(
+            [ActivityTrigger] string rootDirectory,
+            ILogger log)
         {
-            log.LogInformation($"Saying hello to {name}.");
-            return $"Hello {name}!";
+            log.LogInformation($"Searching for files under '{rootDirectory}'...");
+            string[] files = Directory.GetFiles(rootDirectory, "*", SearchOption.AllDirectories);
+            log.LogInformation($"Found {files.Length} file(s) under {rootDirectory}.");
+
+            return files;
         }
 
-        [FunctionName("Function1_HttpStart")]
+        [FunctionName("E2_CopyFileToBlob")]
+        public static async Task<long> CopyFileToBlob(
+            [ActivityTrigger] string filePath,
+            Binder binder,
+            ILogger log)
+        {
+            long byteCount = new FileInfo(filePath).Length;
+
+            // strip the drive letter prefix and convert to forward slashes
+            string blobPath = filePath
+                .Substring(Path.GetPathRoot(filePath).Length)
+                .Replace('\\', '/');
+            string outputLocation = $"backups/{blobPath}";
+
+            log.LogInformation($"Copying '{filePath}' to '{outputLocation}'. Total bytes = {byteCount}.");
+
+            // copy the file contents into a blob
+            using (Stream source = File.Open(filePath, FileMode.Open, FileAccess.Read, FileShare.Read))
+            using (Stream destination = await binder.BindAsync<CloudBlobStream>(
+                new BlobAttribute(outputLocation, FileAccess.Write)))
+            {
+                await source.CopyToAsync(destination);
+            }
+
+            return byteCount;
+        }
+
+        [FunctionName("E2_BackupSiteContent_HttpStart")]
         public static async Task<HttpResponseMessage> HttpStart(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestMessage req,
             [DurableClient] IDurableOrchestrationClient starter,
             ILogger log)
         {
             // Function input comes from the request content.
-            string instanceId = await starter.StartNewAsync("Function1", null);
+            string instanceId = await starter.StartNewAsync("E2_BackupSiteContent", null);
 
             log.LogInformation($"Started orchestration with ID = '{instanceId}'.");
 
